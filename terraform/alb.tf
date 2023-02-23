ab5e6351bf0bc20f839fc3eca96bbcd56af1cd72
Michael Fleming <michael.fleming@nih.gov>
2023-02-23 10:08:48 -0500

    added alb resources

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -1,87 +1,55 @@
 # data
-data "aws_iam_policy_document" "s3_alb_policy" {
-  statement {
-    sid = "allowalbaccount"
-    effect = "Allow"
-    principals {
-      identifiers = ["arn:aws:iam::${lookup(var.aws_account_id,var.region,"us-east-1" )}:root"]
-      type        = "AWS"
-    }
-    actions = ["s3:PutObject"]
-    resources = ["arn:aws:s3:::${module.s3.bucket_name}/*"]
-  }
-  statement {
-    sid = "allowalblogdelivery"
-    effect = "Allow"
-    principals {
-      identifiers = ["delivery.logs.amazonaws.com"]
-      type        = "Service"
-    }
-    actions = ["s3:PutObject"]
-    resources = ["arn:aws:s3:::${module.s3.bucket_name}/*"]
-    condition {
-      test     = "StringEquals"
-      values   = ["bucket-owner-full-control"]
-      variable = "s3:x-amz-acl"
-    }
-  }
-  statement {
-    sid = "awslogdeliveryacl"
-    effect = "Allow"
-    actions = ["s3:GetBucketAcl"]
-    resources = ["arn:aws:s3:::${module.s3.bucket_name}"]
-    principals {
-      identifiers = ["delivery.logs.amazonaws.com"]
-      type        = "Service"
-    }
-  }
+data "aws_acm_certificate" "amazon_issued" {
+  domain      = var.certificate_domain_name
+  types       = [local.cert_types]
+  most_recent = true
 }
 
 # locals
 locals {
-  alb_log_bucket_name = "${var.project}-${terraform.workspace}-alb-access-logs"
-  alb_subnet_ids = terraform.workspace == "prod" || terraform.workspace == "stage" ? var.public_subnet_ids : var.private_subnet_ids
-  application_url =  terraform.workspace == "prod" ? var.domain_name : "${var.application_subdomain}-${terraform.workspace}.${var.domain_name}"
+  alb_subnet_ids      = terraform.workspace == "prod" || terraform.workspace == "stage" ? var.public_subnet_ids : var.private_subnet_ids
+  alb_log_bucket_name = terraform.workspace == "prod" || terraform.workspace == "stage" ? "prod-alb-access-logs" : "nonprod-alb-access-logs"
+  cert_types          = "IMPORTED"
 }
 
 # vars
-variable "application_subdomain" {
-  description = "subdomain of the app"
-  type = string
+variable "certificate_domain_name" {
+  description = "domain name for the ssl cert"
+  type        = string
 }
 
-variable "domain_name" {
-  description = "domain name for the application"
-  type = string
+variable "public_subnet_ids" {
+  description = "Provide list of public subnets to use in this VPC. Example 10.0.1.0/24,10.0.2.0/24"
+  type        = list(string)
 }
 
 variable "s3_force_destroy" {
   description = "force destroy bucket"
-  default = true
-  type = bool
+  default     = true
+  type        = bool
 }
 
 # modules
 module "alb" {
-  source = "./modules/alb"
-  vpc_id = var.vpc_id
+  source              = "./modules/alb"
+  vpc_id              = var.vpc_id
   alb_log_bucket_name = module.s3.bucket_name
-  env = terraform.workspace
-  alb_subnet_ids = local.alb_subnet_ids
-  tags = var.tags
-  project = var.project
+  env                 = terraform.workspace
+  alb_subnet_ids      = local.alb_subnet_ids
+  tags                = var.tags
+  project             = var.project
   alb_certificate_arn = data.aws_acm_certificate.amazon_issued.arn
 }
 
 module "s3" {
-  source = "./modules/s3"
-  bucket_name = local.alb_log_bucket_name
-  project = var.project
-  env = terraform.workspace
-  tags = var.tags
-  s3_force_destroy = var.s3_force_destroy
-  days_for_archive_tiering = 125
+  source                        = "./modules/s3"
+  bucket_name                   = local.alb_log_bucket_name
+  project                       = var.project
+  env                           = terraform.workspace
+  tags                          = var.tags
+  s3_force_destroy              = var.s3_force_destroy
+  days_for_archive_tiering      = 125
   days_for_deep_archive_tiering = 180
-  s3_enable_access_logging = false
-  s3_access_log_bucket_id = ""
+  s3_enable_access_logging      = false
+  s3_access_log_bucket_id       = ""
 }
--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -12,9 +12,9 @@ variable "create_ecr_repos" {
 
 # modules
 module "ecr" {
-  count                    = var.create_ecr_repos ? 1 : 0
-  source                   = "./modules/ecr"
-  project                  = var.project
-  ecr_repo_names           = var.ecr_repo_names
-  tags                     = var.tags
+  count          = var.create_ecr_repos ? 1 : 0
+  source         = "./modules/ecr"
+  project        = var.project
+  ecr_repo_names = var.ecr_repo_names
+  tags           = var.tags
 }
