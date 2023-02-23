894da3dfb421ed108ebab43e2e682abd2b68bb38
Michael Fleming <michael.fleming@nih.gov>
2023-02-23 10:17:44 -0500

    updated vars

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -19,40 +19,10 @@ variable "region" {
   default     = "us-east-1"
 }
 
-#variable "certificate_domain_name" {
-#  description = "domain name for the ssl cert"
-#  type = string
-#}
-
-#variable "internal_alb" {
-#  description = "is this alb internal?"
-#  default = false
-#  type = bool
-#}
-
-#variable "lb_type" {
-#  description = "Type of loadbalancer"
-#  type = string
-#  default = "application"
-#}
-
-#variable "aws_account_id" {
-#  type = map(string)
-#  description = "aws account to allow for alb s3 logging"
-#  default = {
-#    us-east-1 = ""
-#  }
-#}
-
-#variable "public_subnet_ids" {
-#  description = "Provide list of public subnets to use in this VPC. Example 10.0.1.0/24,10.0.2.0/24"
-#  type = list(string)
-#}
-
-#variable "private_subnet_ids" {
-#  description = "Provide list private subnets to use in this VPC. Example 10.0.10.0/24,10.0.11.0/24"
-#  type = list(string)
-#}
+variable "private_subnet_ids" {
+  description = "Provide list private subnets to use in this VPC. Example 10.0.10.0/24,10.0.11.0/24"
+  type = list(string)
+}
 
 #variable "attach_bucket_policy" {
 #  description = "set to true if you want bucket policy and provide value for policy variable"
