9321d5c16f484c8f0511975f837752f7ba1352b3
Michael Fleming <michael.fleming@nih.gov>
2023-03-09 20:03:25 -0500

    updated settings file permissions

--- docker/cicrc-entrypoint.sh
+++ docker/cicrc-entrypoint.sh
@@ -7,3 +7,5 @@ cp web/sites/default/settings.php.template web/sites/default/settings.php \
  && sed -i "s/{{db_user}}/$DB_USER/g" web/sites/default/settings.php \
  && sed -i "s/{{db_pass}}/$DB_PASS/g" web/sites/default/settings.php \
  && sed -i "s/{{db_host}}/$DB_HOST/g" web/sites/default/settings.php
+ 
+chown www-data:www-data web/sites/default/settings.php
