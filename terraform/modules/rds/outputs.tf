caefbd0982133f3238e039e1d4bbd15c491dd2dc
Michael Fleming <michael.fleming@nih.gov>
2023-03-17 16:13:46 -0400

    updated to remove count

--- terraform/modules/rds/outputs.tf
+++ terraform/modules/rds/outputs.tf
@@ -1,10 +1,10 @@
 output "db_instance_endpoint" {
   description = "The connection endpoint"
-  value       = try(aws_db_instance.rds_mysql[0].endpoint, "")
+  value       = try(aws_db_instance.rds_mysql.endpoint, "")
 }
 
 output "db_instance_password" {
   description = "The master password"
-  value       = try(aws_db_instance.rds_mysql[0].password, "")
+  value       = try(aws_db_instance.rds_mysql.password, "")
 #  sensitive   = true
 }
