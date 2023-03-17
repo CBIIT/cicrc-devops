e92e477d3b96030088a1a6a65eceb94d3d2015c6
Michael Fleming <michael.fleming@nih.gov>
2023-03-17 10:41:47 -0400

    commented outputs

--- terraform/modules/rds/outputs.tf
+++ terraform/modules/rds/outputs.tf
@@ -1,17 +1,17 @@
-output "db_instance_address" {
-  description = "The address of the RDS instance"
-  value       = try(aws_db_instance.rds_instance[0].address, "")
-}
-
-output "db_instance_arn" {
-  description = "The ARN of the RDS instance"
-  value       = try(aws_db_instance.rds_instance[0].arn, "")
-}
-
-output "db_instance_endpoint" {
-  description = "The connection endpoint"
-  value       = try(aws_db_instance.rds_instance[0].endpoint, "")
-}
+#output "db_instance_address" {
+#  description = "The address of the RDS instance"
+#  value       = try(aws_db_instance.rds_instance[0].address, "")
+#}
+#
+#output "db_instance_arn" {
+#  description = "The ARN of the RDS instance"
+#  value       = try(aws_db_instance.rds_instance[0].arn, "")
+#}
+#
+#output "db_instance_endpoint" {
+#  description = "The connection endpoint"
+#  value       = try(aws_db_instance.rds_instance[0].endpoint, "")
+#}
 
 ################################################################################
 # CloudWatch Log Group
