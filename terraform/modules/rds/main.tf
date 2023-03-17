ef454578c640df0127ea7d746c85378c443a4b88
Michael Fleming <michael.fleming@nih.gov>
2023-03-17 13:10:11 -0400

    fixed missing call

--- terraform/modules/rds/main.tf
+++ terraform/modules/rds/main.tf
@@ -10,7 +10,7 @@ resource "aws_db_instance" "rds_mysql" {
   parameter_group_name = "default.mysql8.0"
   db_subnet_group_name = "dev-vpc-sngrp"
   skip_final_snapshot  = true
-  vpc_security_group_ids = var.db_security_group
+  vpc_security_group_ids = var.db_security_groups
   
   backup_retention_period = 7
   copy_tags_to_snapshot = true
--- terraform/rds.tf
+++ terraform/rds.tf
@@ -1,4 +1,5 @@
 # modules
 module "rds" {
   source         = "./modules/rds"
+  db_security_groups = var.db_security_groups
 }
