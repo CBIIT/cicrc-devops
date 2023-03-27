68d7036a39c8e93d74ee57cc331c9eba8cfad0a3
Michael Fleming <michael.fleming@nih.gov>
2023-03-27 15:42:21 -0400

    updated security groups to allow traffic to alb from all ip ranges

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -17,8 +17,8 @@ locals {
   any_protocol = "-1"
   tcp_protocol = "tcp"
   https_port   = "443"
-  nih_ip_cidrs =  terraform.workspace == "prod" || terraform.workspace == "stage" ? ["0.0.0.0/0"] : [ "129.43.0.0/16" , "137.187.0.0/16"  , "165.112.0.0/16" , "156.40.0.0/16"  , "128.231.0.0/16" , "130.14.0.0/16" , "157.98.0.0/16"]
-  #nih_ip_cidrs = ["0.0.0.0/0"]
+  #nih_ip_cidrs =  terraform.workspace == "prod" || terraform.workspace == "stage" ? ["0.0.0.0/0"] : [ "129.43.0.0/16" , "137.187.0.0/16"  , "165.112.0.0/16" , "156.40.0.0/16"  , "128.231.0.0/16" , "130.14.0.0/16" , "157.98.0.0/16"]
+  nih_ip_cidrs = ["0.0.0.0/0"]
   all_ips      =  ["0.0.0.0/0"]
   allowed_alb_ip_range = terraform.workspace == "prod" || terraform.workspace == "stage" ?  local.all_ips : local.nih_ip_cidrs
   #allowed_alb_ip_range         = local.nih_ip_cidrs
