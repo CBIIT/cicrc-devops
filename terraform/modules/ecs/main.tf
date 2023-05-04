c10a96e3d9de1a053d9c46d33448ffc20f7a8527
Michael Fleming <michael.fleming@nih.gov>
2023-05-04 17:33:24 -0400

    added exec command

--- terraform/modules/ecs/main.tf
+++ terraform/modules/ecs/main.tf
@@ -35,6 +35,7 @@ resource "aws_ecs_task_definition" "task" {
 resource "aws_ecs_service" "service" {
   for_each                           = var.microservices
   name                               = "${var.project}-${var.env}-${each.value.name}"
+  enable_execute_command             = true
   cluster                            = aws_ecs_cluster.ecs_cluster.id
   task_definition                    = aws_ecs_task_definition.task[each.key].arn
   desired_count                      = each.value.number_container_replicas
