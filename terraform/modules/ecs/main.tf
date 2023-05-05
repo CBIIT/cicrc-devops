d457174c2c9c06289c4c3ce8204671c89fa2f4db
Michael Fleming <michael.fleming@nih.gov>
2023-05-05 06:56:37 -0400

    set the cloudwatch log encryption rules

--- terraform/modules/ecs/main.tf
+++ terraform/modules/ecs/main.tf
@@ -110,7 +110,7 @@ resource "aws_ecs_cluster" "ecs_cluster" {
 
       log_configuration {
         cloud_watch_log_group_name     = aws_cloudwatch_log_group.ecs_execute_command_log_group.name
-        cloud_watch_encryption_enabled = true
+        cloud_watch_encryption_enabled = false
       }
     }
   }
