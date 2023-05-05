52614339102ee4167bbb40e09d3bf72c8c64f2ab
Michael Fleming <michael.fleming@nih.gov>
2023-05-05 11:28:40 -0400

    moved ecs log group config

--- terraform/modules/ecs/cloudwatch.tf
+++ terraform/modules/ecs/cloudwatch.tf
@@ -2,3 +2,14 @@ resource "aws_cloudwatch_log_group" "ecs_execute_command_log_group" {
   name              = local.ecs_exec_log_group
   retention_in_days = 90
 }
+
+resource "aws_cloudwatch_log_group" "ecs_log_group" {
+  for_each = var.microservices
+  name     = "${var.project}/ecs/${var.env}/${each.value.name}"
+  retention_in_days = each.value.log_retention
+
+  tags = {
+    Environment = "${var.env}"
+    Project     = "CICRC"
+  }
+}
