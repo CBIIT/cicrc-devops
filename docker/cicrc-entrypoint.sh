c470b43e293f306c1530355d681c1e75fc595235
Michael Fleming <michael.fleming@nih.gov>
2023-05-09 13:20:51 -0400

    added drush commands

--- docker/cicrc-entrypoint.sh
+++ docker/cicrc-entrypoint.sh
@@ -9,3 +9,10 @@ cp web/sites/default/settings.php.template web/sites/default/settings.php \
  && sed -i "s/{{db_host}}/$DB_HOST/g" web/sites/default/settings.php
  
 chown www-data:www-data web/sites/default/settings.php
+
+# Run drush commands
+vendor/bin/drush cr \
+ && vendor/bin/drush cim -y \
+ && vendor/bin/drush cr \
+ && vendor/bin/drush updb -y \
+ && vendor/bin/drush cr
