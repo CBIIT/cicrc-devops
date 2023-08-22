01b613902da9097e15888f1c2802976c9159c856
Fleming, Michael (NIH/NCI) [C] <michael.fleming@nih.gov>
2023-08-22 08:04:33 -0400

    reverted debug

--- docker/cicrc-entrypoint.sh
+++ docker/cicrc-entrypoint.sh
@@ -8,11 +8,6 @@ cp web/sites/default/settings.php.template web/sites/default/settings.php \
  && sed -i "s/{{db_pass}}/$DB_PASS/g" web/sites/default/settings.php \
  && sed -i "s/{{db_host}}/$DB_HOST/g" web/sites/default/settings.php
 
-##################  DEBUG  ##############################
-sed -i 's|LogLevel warn|LogLevel debug|g' /etc/apache2/apache2.conf
-sed -i 's|ErrorLog ${APACHE_LOG_DIR}/error.log|ErrorLog ${APACHE_LOG_DIR}/test.log|g' /etc/apache2/apache2.conf
-#########################################################
-
 chown www-data:www-data web/sites/default/settings.php
 
 # Run drush commands
