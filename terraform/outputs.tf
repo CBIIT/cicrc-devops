a9f4efcae73047ff7b2f47e2f2c44291e1721e46
moshafis2 <saman.moshafi@nih.gov>
2024-05-02 17:51:24 -0400

    CRCR-837 there was no reason for displaying the secrets

--- terraform/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "db_password" {
-  value     = try(module.rds.db_instance_password, "")
-  sensitive = true
-}
-
-output "random_password" {
-  value = try(module.rds.db_random_password, "")
-}
