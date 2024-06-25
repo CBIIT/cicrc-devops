317fbf86c3100bae959d488bba18f64284985070
Sowmya Karavadi <ksowmya442@gmail.com>
2024-06-25 08:59:01 -0400

    updating based on errors on container bringup

--- docker/crcr-entrypoint.sh
+++ docker/crcr-entrypoint.sh
@@ -1,23 +1,23 @@
 # move default config file
-mv web/sites/default/settings.php web/sites/default/settings.php.bak
+mv sites/default/settings.php web/sites/default/settings.php.bak
 
 # update the db credentials in the config file
-cp web/sites/default/settings.php.template web/sites/default/settings.php \
- && sed -i "s/{{db_name}}/$DB_NAME/g" web/sites/default/settings.php \
- && sed -i "s/{{db_user}}/$DB_USER/g" web/sites/default/settings.php \
- && sed -i "s/{{db_pass}}/$DB_PASS/g" web/sites/default/settings.php \
- && sed -i "s/{{db_host}}/$DB_HOST/g" web/sites/default/settings.php
+cp sites/default/settings.php.template sites/default/settings.php \
+ && sed -i "s/{{db_name}}/$DB_NAME/g" sites/default/settings.php \
+ && sed -i "s/{{db_user}}/$DB_USER/g" sites/default/settings.php \
+ && sed -i "s/{{db_pass}}/$DB_PASS/g" sites/default/settings.php \
+ && sed -i "s/{{db_host}}/$DB_HOST/g" sites/default/settings.php
 
-chown www-data:www-data web/sites/default/settings.php
+chown www-data:www-data sites/default/settings.php
 
 # Run drush commands
-vendor/bin/drush cr \
- && vendor/bin/drush cim -y \
- && vendor/bin/drush cr \
- && vendor/bin/drush updb -y \
- && vendor/bin/drush cr
- && vendor/bin/drush cim -y \
- && vendor/bin/drush cr \
- && vendor/bin/drush sapi-c -y \
- && vendor/bin/drush sapi-i -y \
- && vendor/bin/drush cr
+../vendor/bin/drush cr \
+ && ../vendor/bin/drush cim -y \
+ && ../vendor/bin/drush cr \
+ && ../vendor/bin/drush updb -y \
+ && ../vendor/bin/drush cr
+ && ../vendor/bin/drush cim -y \
+ && ../vendor/bin/drush cr \
+ && ../vendor/bin/drush sapi-c -y \
+ && ../vendor/bin/drush sapi-i -y \
+ && ../vendor/bin/drush cr
