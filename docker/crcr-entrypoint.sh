4440be4b0bb73dfa7975c3815abe897a1a8b25f7
Sowmya Karavadi <ksowmya442@gmail.com>
2024-07-03 13:40:36 -0400

    updated files for permissions

--- docker/crcr-entrypoint.sh
+++ docker/crcr-entrypoint.sh
@@ -1,23 +1,26 @@
 # move default config file
-mv sites/default/settings.php sites/default/settings.php.bak
+mv web/sites/default/settings.php web/sites/default/settings.php.bak
 
 # update the db credentials in the config file
-cp sites/default/settings.php.template sites/default/settings.php \
- && sed -i "s/{{db_name}}/$DB_NAME/g" sites/default/settings.php \
- && sed -i "s/{{db_user}}/$DB_USER/g" sites/default/settings.php \
- && sed -i "s/{{db_pass}}/$DB_PASS/g" sites/default/settings.php \
- && sed -i "s/{{db_host}}/$DB_HOST/g" sites/default/settings.php
+cp web/sites/default/settings.php.template web/sites/default/settings.php \
+ && sed -i "s/{{db_name}}/$DB_NAME/g" web/sites/default/settings.php \
+ && sed -i "s/{{db_user}}/$DB_USER/g" web/sites/default/settings.php \
+ && sed -i "s/{{db_pass}}/$DB_PASS/g" web/sites/default/settings.php \
+ && sed -i "s/{{db_host}}/$DB_HOST/g" web/sites/default/settings.php
 
-chown www-data:www-data sites/default/settings.php
+chown www-data:www-data web/sites/default/settings.php
+
+# Set the Drupal root
+DRUPAL_ROOT="/var/www/html/web"
 
 # Run drush commands
-../vendor/bin/drush cr \
- && ../vendor/bin/drush cim -y \
- && ../vendor/bin/drush cr \
- && ../vendor/bin/drush updb -y \
- && ../vendor/bin/drush cr \
- && ../vendor/bin/drush cim -y \
- && ../vendor/bin/drush cr \
- && ../vendor/bin/drush sapi-c -y \
- && ../vendor/bin/drush sapi-i -y \
- && ../vendor/bin/drush cr
+vendor/bin/drush --root="$DRUPAL_ROOT" cr \
+ && vendor/bin/drush --root="$DRUPAL_ROOT" cim -y \
+ && vendor/bin/drush --root="$DRUPAL_ROOT" cr \
+ && vendor/bin/drush --root="$DRUPAL_ROOT" updb -y \
+ && vendor/bin/drush --root="$DRUPAL_ROOT" cr \
+ && vendor/bin/drush --root="$DRUPAL_ROOT" cim -y \
+ && vendor/bin/drush --root="$DRUPAL_ROOT" cr \
+ && vendor/bin/drush --root="$DRUPAL_ROOT" sapi-c -y \
+ && vendor/bin/drush --root="$DRUPAL_ROOT" sapi-i -y \
+ && vendor/bin/drush --root="$DRUPAL_ROOT" cr
