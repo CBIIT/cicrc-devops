4c16686b5dd657288f12497a4b7b7fb1ef1156d4
Sowmya Karavadi <ksowmya442@gmail.com>
2024-07-12 13:35:40 -0400

    updated docker

--- docker/crcr-entrypoint.sh
+++ docker/crcr-entrypoint.sh
@@ -10,17 +10,14 @@ cp web/sites/default/settings.php.template web/sites/default/settings.php \
 
 chown www-data:www-data web/sites/default/settings.php
 
-# Set the Drupal root
-DRUPAL_ROOT="/var/www/html/web"
-
 # Run drush commands
-vendor/bin/drush --root="$DRUPAL_ROOT" cr \
- && vendor/bin/drush --root="$DRUPAL_ROOT" cim -y \
- && vendor/bin/drush --root="$DRUPAL_ROOT" cr \
- && vendor/bin/drush --root="$DRUPAL_ROOT" updb -y \
- && vendor/bin/drush --root="$DRUPAL_ROOT" cr \
- && vendor/bin/drush --root="$DRUPAL_ROOT" cim -y \
- && vendor/bin/drush --root="$DRUPAL_ROOT" cr \
- && vendor/bin/drush --root="$DRUPAL_ROOT" sapi-c -y \
- && vendor/bin/drush --root="$DRUPAL_ROOT" sapi-i -y \
- && vendor/bin/drush --root="$DRUPAL_ROOT" cr
+vendor/bin/drush cr \
+ && vendor/bin/drush cim -y \
+ && vendor/bin/drush cr \
+ && vendor/bin/drush updb -y \
+ && vendor/bin/drush cr \
+ && vendor/bin/drush cim -y \
+ && vendor/bin/drush cr \
+ && vendor/bin/drush sapi-c -y \
+ && vendor/bin/drush sapi-i -y \
+ && vendor/bin/drush cr
