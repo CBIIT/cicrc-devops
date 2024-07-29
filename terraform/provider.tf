92061616c52084e330536277105fefcba2922861
Sowmya Karavadi <ksowmya442@gmail.com>
2024-07-29 11:15:04 -0400

    updated missing required param in rds and providers

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -9,4 +9,22 @@ terraform {
 
 provider "aws" {
   region = var.region
+
+  default_tags {
+    tags = {
+      EnvironmentTier = terraform.workspace
+      DevLead         = "Saman Moshafi"
+      CreatedBy       = "sowmya karavadi"
+      ResourceName    = "NCI-CRCR-${terraform.workspace}"
+      FISMA           = "moderate"
+      ManagedBy       = "terraform"
+      OpsModel        = "cbiit managed hybrid"
+      Program         = "nci-doe"
+      PII             = "yes"
+      Backup          = local.level
+      Project         = "crcr"
+      ApplicationName = "CRCR"
+      ProjectManager  = "Saman Moshafi"
+    }
+  }
 }
